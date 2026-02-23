93cbd781c116e7c76af26abea0d422205793fa77
doomsdie008 <real.aryaman@gmail.com>
2026-02-23 08:50:06 +0530

    Gawtham Added usecase2

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -3,5 +3,6 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args){
         System.out.println("Welcome To Palindrome App");
+        System.out.println("Usecase 2 logic");
     }
 }
